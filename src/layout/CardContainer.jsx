6298835b79dedd726c913114429f2d7a75435b0d
Zhen Yang <webyangfullstack@gmail.com>
2022-09-03 12:20:51 +1000

    build nav

--- src/layout/CardContainer.jsx
+++ src/layout/CardContainer.jsx
@@ -1,46 +1,48 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 const CardContainer = (props)=> {
+	const [activeItem, setActiveItem] = useState("home");
 	return (
     <>
       <div className="card-container">
         <ul className="card-nav">
           <li className="card-nav_nav-item">
-            <a href="#home" className="active">
+            <a href="#home" className={activeItem === "home" ? "active" : ""} onClick={()=>setActiveItem('home')}>
               <span className="menu-icon lnr lnr-home"></span>
-			  <span className="hover-tab">Home</span>
+              <span className="hover-tab">Home</span>
             </a>
           </li>
           <li className="card-nav_nav-item">
-            <a href="#about-me">
+            <a href="#about-me" className={activeItem === "about-me" ? "active" : ""} onClick={()=>setActiveItem('about-me')}>
               <span className="menu-icon lnr lnr-user"></span>
-			  <span className="hover-tab">About me</span>
+              <span className="hover-tab">About me</span>
             </a>
           </li>
           <li className="card-nav_nav-item">
-            <a href="#resume">
+            <a href="#resume" className={activeItem === "resume" ? "active" : ""} onClick={()=>setActiveItem('resume')}>
               <span className="menu-icon lnr lnr-graduation-hat"></span>
-			  <span className="hover-tab">Resume</span>
+              <span className="hover-tab">Resume</span>
             </a>
           </li>
           <li className="card-nav_nav-item">
-            <a href="#portfolio">
+            <a href="#portfolio" className={activeItem === "portfolio" ? "active" : ""} onClick={()=>setActiveItem('portfolio')}>
               <span className="menu-icon lnr lnr-briefcase"></span>
-			  <span className="hover-tab">Portfolio</span>
+              <span className="hover-tab">Portfolio</span>
             </a>
           </li>
           <li className="card-nav_nav-item">
-            <a href="#blog">
+            <a href="#blog" className={activeItem === "blog" ? "active" : ""} onClick={()=>setActiveItem('blog')}>
               <span className="menu-icon lnr lnr-book"></span>
-			  <span className="hover-tab">Blog</span>
+              <span className="hover-tab">Blog</span>
             </a>
           </li>
-		  <li className="card-nav_nav-item">
-			<a href="#contact">
-				<span className="menu-icon lnr lnr-envelope"></span>
-				<span className="hover-tab">Contact</span>
-			</a>
-		  </li>
+          <li className="card-nav_nav-item">
+            <a href="#contact" className={activeItem === "contact" ? "active" : ""} onClick={()=>setActiveItem('contact')}>
+              <span className="menu-icon lnr lnr-envelope"></span>
+              <span className="hover-tab">Contact</span>
+            </a>
+          </li>
         </ul>
         <div className="toggle-nav"></div>
       </div>
